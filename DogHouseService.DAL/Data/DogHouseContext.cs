3c3afb9c1aa81412dfb04c3a2e38127540615cc8
Ponomarov_Oleksii <ponomarov.oleksiii@gmail.com>
2024-10-27 15:21:29 +0200

    init

---
+++ DogHouseService.DAL/Data/DogHouseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DogHouseService.DAL.Models;
+
+namespace DogHouseService.DAL.Data
+{
+    public class DogHouseContext : DbContext
+    {
+        public DogHouseContext(DbContextOptions<DogHouseContext> options) : base(options) { }
+
+        public DbSet<Dog> Dogs { get; set; }
+    }
+}
---
+++ DogHouseService.DAL/Models/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogHouseService.DAL.Models
+{
+    public class Dog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Color { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int TailLength { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Weight { get; set; }
+    }
+}
