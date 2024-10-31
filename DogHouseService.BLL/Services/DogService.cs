550e14e6cc3be31769ce8f99a42cafa10d1cad0e
Ponomarov_Oleksii <ponomarov.oleksiii@gmail.com>
2024-10-31 15:41:53 +0200

    fix xml comments

--- DogHouseService.BLL/Services/DogService.cs
+++ DogHouseService.BLL/Services/DogService.cs
@@ -18,16 +18,7 @@ public class DogService : IDogService
         _context = context;
         _mapper = mapper;
     }
-
-    /// <summary>
-    /// Asynchronously retrieves a paginated list of dogs from the database, with optional sorting by a specified attribute.
-    /// </summary>
-    /// <param name="attribute">The attribute to sort by, such as "Name", "Color", "TailLength" or "Weight".</param>
-    /// <param name="order">The order of sorting, either "asc" for ascending or "desc" for descending.</param>
-    /// <param name="pageNumber">The page number for pagination.</param>
-    /// <param name="pageSize">The number of items per page.</param>
-    /// <returns>A task representing the asynchronous operation, containing a paginated and sorted list of <see cref="DogModel"/> instances.</returns>
-
+   
     public async Task<IEnumerable<DogModel>> GetDogsAsync(string attribute = null, string order = null, int pageNumber = 1, int pageSize = 10)
     {
         var query = _context.Dogs.AsQueryable();
@@ -44,15 +35,7 @@ public class DogService : IDogService
 
         return _mapper.Map<IEnumerable<DogModel>>(dogs);
     }
-
-    /// <summary>
-    /// Asynchronously creates a new dog entry in the database based on the provided <see cref="DogModel"/>.
-    /// </summary>
-    /// <param name="dogModel">The dog model containing information about the dog to be created.</param>
-    /// <returns>A task that represents the asynchronous operation. The task result contains the created <see cref="DogModel"/>.</returns>
-    /// <exception cref="InvalidOperationException">
-    /// Thrown when a dog with the same name already exists or if the tail length is a negative value.
-    /// </exception>
+   
     public async Task<DogModel> CreateDogAsync(DogModel dogModel)
     {
         if (await _context.Dogs.AnyAsync(d => d.Name == dogModel.Name))
